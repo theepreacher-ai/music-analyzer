add1124ae2cbd6f46a8ea5b7c37b3e711a728579
frank97 <kuriaf791@gmail.com>
2026-01-10 21:28:21 -0700

    Update video-audio-extractor.js

--- music-analyzer/assets/js/video-audio-extractor.js
+++ music-analyzer/assets/js/video-audio-extractor.js
@@ -1,1 +1,9 @@
 // Extract audio from video
+export class VideoAudioExtractor {
+    constructor(audioContext) {
+        this.audioContext = audioContext;
+    }
+    
+    async extractAudio(videoFile) {
+        return new Promise((resolve, reject) => {
+            const video
